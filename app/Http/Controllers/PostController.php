0675fc4214fc7a63fbf6c18bd0b79450085de3a1
yuu4jp <youmayuko.you@gmail.com>
2023-11-25 19:16:39 +0900

    カリキュラム８－４課題３完了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,21 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+        return view('posts.index')->with(['posts'=> $post->getPaginateByLimit()]);
+    }
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}',[PostController::class,'show']);
+Route::post('/posts', [PostController::class, 'store']);
+
 /*Route::get('/', function () {
     return view('welcome');
 });*/
