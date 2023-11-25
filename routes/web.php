39c1fbe31e2910e6238e31f01f70887ded63fecf
yuu4jp <youmayuko.you@gmail.com>
2023-11-25 22:30:38 +0900

    カリキュラム８－５終了

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}',[PostController::class,'show']);
 Route::post('/posts', [PostController::class, 'store']);
-
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
 /*Route::get('/', function () {
     return view('welcome');
 });*/
