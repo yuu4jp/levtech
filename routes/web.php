6a902bade70787101be95c061ef21a96eae5f539
yuu4jp <youmayuko.you@gmail.com>
2023-11-30 22:38:00 +0900

    カリキュラム８－６終了

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/posts/{post}',[PostController::class,'show']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
 /*Route::get('/', function () {
     return view('welcome');
 });*/
